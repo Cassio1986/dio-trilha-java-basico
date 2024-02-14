dabd930980654d1d2c9ae80053d3842b4350163a
Cássio Fernandes <cassio.fernandes.silva@outlook.com>
2024-02-14 17:13:21 -0300

    Pegando hora

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String dataAtualFormatada = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(System.currentTimeMillis());
+        String mensagem = "Bom dia Lara! ";
+        int hora = Integer.parseInt(dataAtualFormatada.substring(11,12));
+        if (hora <= 12){
+        mensagem = "Boa tarde lara";
+        }
+
+        System.out.println(mensagem);
     }
 }
