28401f784f5bdf874c7e92af418f502b94f84763
Cássio Fernandes <cassio.fernandes.silva@outlook.com>
2024-02-15 20:49:43 -0300

    Saudação

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,12 +4,16 @@ import java.time.format.DateTimeFormatter;
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        
+
         String dataAtualFormatada = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(System.currentTimeMillis());
+
         String mensagem = "Bom dia Lara! ";
-        int hora = Integer.parseInt(dataAtualFormatada.substring(11,12));
-        if (hora <= 12){
-        mensagem = "Boa tarde lara";
+        int hora = Integer.parseInt(dataAtualFormatada.substring(11, 13));
+
+        if ((hora <= 12) && (hora > 18)) {
+            mensagem = "Boa tarde Lara";
+        } else if (hora >= 18) {
+            mensagem = "Boa noite Lara";
         }
 
         System.out.println(mensagem);
