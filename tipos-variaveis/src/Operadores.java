f898663b627f1b7db41f8cdb98d532bd95815fc4
Cássio Fernandes <cassio.fernandes.silva@outlook.com>
2024-02-15 22:57:10 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    
+public static void main(String[] args) {
+    
+int numero1 = 2;
+int numero2 = 2;
+
+boolean simNao = numero1 ==  numero2;
+
+
+    System.out.println("Numero Um é igual oa numero Dois? " + simNao) ;
+}
+
+}
